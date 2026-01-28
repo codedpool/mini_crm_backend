e3b4596fa54304dbeb71d6138b1823747cf8853f
codedpool <codedpool10@gmail.com>
2026-01-28 10:09:51 +0530

    initializing app

---
+++ src/app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const morgan = require('morgan');
+const swaggerUi = require('swagger-ui-express');
+const swaggerJsDoc = require('swagger-jsdoc');
+
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan('dev'));
+
+// Swagger Config
+const swaggerOptions = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Mini CRM API',
+      version: '1.0.0',
+      description: 'Backend API for CRM system (Node.js/Express)',
+    },
+    servers: [{ url: 'http://localhost:3000' }],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+          bearerFormat: 'JWT',
+        },
+      },
+    },
+    security: [{ bearerAuth: [] }],
+  },
+  apis: ['./src/routes/*.js'], // Path to the API docs
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+// Basic Route for testing
+app.get('/', (req, res) => {
+  res.json({ message: 'Mini CRM Backend is running!' });
+});
+
+module.exports = app;
